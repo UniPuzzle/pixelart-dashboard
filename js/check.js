6782b37f04eae6e3b73f8070d152fd1672a43aad
VitaliiChichuga <chichygav@gmail.com>
2023-07-28 14:46:41 +0300

    add
    1. Displays the link text based on the link
    2. Copy link on button click

--- js/check.js
+++ js/check.js
@@ -1,17 +1,28 @@
+const getHref = document.querySelector('.dashboard-title-link').href;
+const getLinkForInnerText = document.querySelector('a.dashboard-title-link');
+
 const copyLinkBtn = document.querySelector('.dashboard-title-copy-link');
 const copyLinkBtnText = document.querySelector(
   '.dashboard-title-copy-link-text',
 );
 const printBtn = document.querySelector('.print-btn');
+const linkText = document.querySelector('.dashboard-title-link');
 const checkAll = document.getElementById('checked-all');
 const checkBoxes = document.querySelectorAll(
   'input.dashboard-input-checked-card',
 );
 
+// ========Displays the link text based on the link
+function displaysLinkTextBasedOnLink() {
+  getLinkForInnerText.textContent = getHref;
+}
+
 // ======Show escape text "Copied" when clicking CopyLink button=====
 copyLinkBtn.onclick = function () {
   copyLinkBtnText.classList.add('copy-link-active');
   setTimeout(() => copyLinkBtnText.classList.remove('copy-link-active'), 2000);
+  // ==========Copy link on button click
+  navigator.clipboard.writeText(linkText.innerHTML);
 };
 
 // ======Selecting one or all checkboxes======
@@ -43,3 +54,7 @@ checkAll.onclick = function () {
     printBtn.classList.remove('print-btn-show');
   }
 };
+
+// ==========
+
+displaysLinkTextBasedOnLink();
